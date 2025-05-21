967062930bddf326b556a42676b603236170ede3
Jani Sillanpää <janzku1993@hotmail.com>
2025-05-21 10:24:21 +0300

    Add hedgehog age, name, gender, location to model & db up script

--- shared/src/hedgehog.ts
+++ shared/src/hedgehog.ts
@@ -4,8 +4,19 @@ import { z } from "zod";
  * Hedgehog interface shared between server and client
  */
 
+const sex = ["Male", "Female", "Unknown"] as const;
+
+const locationSchema = z.object({
+  lon: z.number().min(-180).max(180),
+  lat: z.number().min(-90).max(90),
+});
+
 export const hedgehogSchema = z.object({
-  id: z.number(), // TODO: loput siilin tietomallista. Zod:lta löytyy esimerkiksi tällaisia tyyppejä: z.enum(), z.string(), z.number() jne. joita voi olla tarpeen hyödyntää
+  id: z.number(),
+  name: z.string().nullish(),
+  age: z.number().nullish(),
+  gender: z.enum(sex),
+  location: locationSchema,
 });
 
 export type Hedgehog = z.infer<typeof hedgehogSchema>;
